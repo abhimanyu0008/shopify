d334b8e3b97b12730f200e6e0c610615c098be58
Shivvardhan <shivvardhan52@gmail.com>
2024-09-01 16:29:20 +0530

    add

--- src/main/java/com/jbk/controller/Demo.java
+++ src/main/java/com/jbk/controller/Demo.java
@@ -1,9 +1,10 @@
 package com.jbk.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Demo {
 	@GetMapping("/hello")
 	public String getC() {
--- src/main/webapp/view/index.jsp
+++ src/main/webapp/view/index.jsp
@@ -7,6 +7,6 @@
 <title>home</title>
 </head>
 <body>
-<h1>This is index page</h1>
+<h1>This is index page and R&D</h1>
 </body>
 </html>
